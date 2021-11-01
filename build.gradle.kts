f8ad293c825f7c73f61830f9919218e862ca24bf
Brandon Fergerson <bfergerson@apache.org>
2021-11-01 14:17:46 -0400

    simplify

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,8 @@ import java.io.FileOutputStream
 import java.net.URL
 
 plugins {
-    id("java")
-    id("com.avast.gradle.docker-compose")
-
-    kotlin("jvm")
-
     id("io.gitlab.arturbosch.detekt")
+    id("com.avast.gradle.docker-compose")
 }
 
 val probeGroup: String by project
@@ -19,69 +15,12 @@ val vertxVersion: String by project
 group = probeGroup
 version = probeVersion
 
-repositories {
-    mavenCentral()
-    jcenter()
-    maven(url = "https://kotlin.bintray.com/kotlinx/")
-}
-
 subprojects {
     repositories {
         mavenCentral()
         jcenter()
         maven(url = "https://kotlin.bintray.com/kotlinx/")
         maven(url = "https://jitpack.io")
-    }
-
-    apply(plugin = "org.jetbrains.kotlin.jvm")
-    if (name == "control" || name == "services" || name == "protocol") return@subprojects
-
-    apply(plugin = "kotlin-kapt")
-
-    dependencies {
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
-        implementation("io.vertx:vertx-core:$vertxVersion")
-        implementation("io.vertx:vertx-lang-kotlin:$vertxVersion")
-        implementation("io.vertx:vertx-lang-kotlin-coroutines:$vertxVersion")
-        implementation("io.vertx:vertx-web:$vertxVersion")
-        implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
-        implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:$jacksonVersion")
-        implementation("com.fasterxml.jackson.datatype:jackson-datatype-guava:$jacksonVersion")
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
-        implementation("io.dropwizard.metrics:metrics-core:4.2.4")
-        implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.3.0")
-    }
-
-    apply<io.gitlab.arturbosch.detekt.DetektPlugin>()
-    tasks {
-        withType<io.gitlab.arturbosch.detekt.Detekt> {
-            parallel = true
-            buildUponDefaultConfig = true
-        }
-
-        withType<JavaCompile> {
-            sourceCompatibility = "1.8"
-            targetCompatibility = "1.8"
-        }
-        withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-            kotlinOptions.apiVersion = "1.4"
-            kotlinOptions.jvmTarget = "1.8"
-            kotlinOptions.freeCompilerArgs +=
-                listOf(
-                    "-Xno-optimized-callable-references",
-                    "-Xjvm-default=compatibility"
-                )
-        }
-
-        withType<Test> {
-            testLogging {
-                events("passed", "skipped", "failed")
-                setExceptionFormat("full")
-
-                outputs.upToDateWhen { false }
-                showStandardStreams = true
-            }
-        }
     }
 }
 
