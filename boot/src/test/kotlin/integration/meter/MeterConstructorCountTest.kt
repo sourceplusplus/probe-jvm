068af96a053f8823d221613135bc5a14d2838934
Brandon Fergerson <bfergerson@apache.org>
2023-01-06 18:24:32 +0400

    feat: ability to add meter to constructors (#180)

---
+++ boot/src/test/kotlin/integration/meter/MeterConstructorCountTest.kt
@@ -0,0 +1,124 @@
+/*
+ * Source++, the continuous feedback platform for developers.
+ * Copyright (C) 2022-2023 CodeBrig, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package integration.meter
+
+import integration.ProbeIntegrationTest
+import io.vertx.core.json.JsonObject
+import io.vertx.junit5.VertxTestContext
+import io.vertx.kotlin.coroutines.await
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Test
+import spp.protocol.artifact.ArtifactQualifiedName
+import spp.protocol.artifact.ArtifactType
+import spp.protocol.instrument.LiveMeter
+import spp.protocol.instrument.LiveSourceLocation
+import spp.protocol.instrument.meter.MeterType
+import spp.protocol.instrument.meter.MetricValue
+import spp.protocol.instrument.meter.MetricValueType
+import spp.protocol.service.SourceServices.Subscribe.toLiveViewSubscriberAddress
+import spp.protocol.view.LiveView
+import spp.protocol.view.LiveViewConfig
+import spp.protocol.view.LiveViewEvent
+import spp.protocol.view.rule.LiveViewRule
+
+class MeterConstructorCountTest : ProbeIntegrationTest() {
+
+    private fun doTest() {
+        repeat(10) {
+            MyObject()
+        }
+    }
+
+    @Test
+    fun `constructor count`(): Unit = runBlocking {
+        val meterId = "constructor-count"
+
+        val liveMeter = LiveMeter(
+            MeterType.COUNT,
+            MetricValue(MetricValueType.NUMBER, "1"),
+            location = LiveSourceLocation(
+                MyObject::class.java.name + ".<init>()",
+                service = "spp-test-probe"
+            ),
+            id = meterId,
+            applyImmediately = true,
+            meta = mapOf("metric.mode" to "RATE")
+        )
+
+        viewService.saveRuleIfAbsent(
+            LiveViewRule(
+                name = liveMeter.toMetricIdWithoutPrefix(),
+                exp = buildString {
+                    append("(")
+                    append(liveMeter.toMetricIdWithoutPrefix())
+                    append(".sum(['service', 'instance'])")
+                    append(".downsampling(SUM)")
+                    append(")")
+                    append(".instance(['service'], ['instance'], Layer.GENERAL)")
+                }
+            )
+        ).await()
+
+        val subscriptionId = viewService.addLiveView(
+            LiveView(
+                entityIds = mutableSetOf(liveMeter.toMetricId()),
+                artifactQualifiedName = ArtifactQualifiedName(
+                    MyObject::class.java.name + ".<init>()",
+                    type = ArtifactType.METHOD
+                ),
+                artifactLocation = LiveSourceLocation(
+                    MyObject::class.java.name + ".<init>()"
+                ),
+                viewConfig = LiveViewConfig(
+                    "test",
+                    listOf(liveMeter.toMetricId())
+                )
+            )
+        ).await().subscriptionId!!
+        val consumer = vertx.eventBus().localConsumer<JsonObject>(
+            toLiveViewSubscriberAddress("system")
+        )
+
+        val testContext = VertxTestContext()
+        consumer.handler {
+            val liveViewEvent = LiveViewEvent(it.body())
+            val rawMetrics = JsonObject(liveViewEvent.metricsData)
+            testContext.verify {
+                val meta = rawMetrics.getJsonObject("meta")
+                assertEquals(liveMeter.toMetricId(), meta.getString("metricsName"))
+
+                assertEquals(10, rawMetrics.getLong("value"))
+            }
+            testContext.completeNow()
+        }.completionHandler().await()
+
+        instrumentService.addLiveInstrument(liveMeter).await()
+
+        doTest()
+
+        errorOnTimeout(testContext)
+
+        //clean up
+        consumer.unregister()
+        assertNotNull(instrumentService.removeLiveInstrument(meterId).await())
+        assertNotNull(viewService.removeLiveView(subscriptionId).await())
+    }
+
+    class MyObject
+}
