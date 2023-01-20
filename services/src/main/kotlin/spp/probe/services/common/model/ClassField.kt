a2910ce7ad57ad1f7b2e4e8f64d797ff4c88ce0a
Brandon Fergerson <bfergerson@apache.org>
2023-01-21 00:11:53 +0400

    refactor: use data class

--- services/src/main/kotlin/spp/probe/services/common/model/ClassField.kt
+++ services/src/main/kotlin/spp/probe/services/common/model/ClassField.kt
@@ -16,15 +16,8 @@
  */
 package spp.probe.services.common.model
 
-import java.io.Serializable
-
-class ClassField(val access: Int, val name: String, val desc: String) : Serializable {
-
-    override fun toString(): String {
-        return "ClassField{" +
-                "access=" + access +
-                ", name='" + name + '\'' +
-                ", desc='" + desc + '\'' +
-                '}'
-    }
-}
+data class ClassField(
+    val access: Int,
+    val name: String,
+    val desc: String
+)
