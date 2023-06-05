cb749ac1a0f0dd1435ff825f38c928a0032aa03a
Brandon Fergerson <bfergerson@apache.org>
2023-06-04 20:34:36 -0500

    test: disable flaky

--- boot/src/test/kotlin/integration/meter/MeterMethodTimerTest.kt
+++ boot/src/test/kotlin/integration/meter/MeterMethodTimerTest.kt
@@ -22,6 +22,7 @@ import io.vertx.junit5.VertxTestContext
 import io.vertx.kotlin.coroutines.await
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import spp.protocol.instrument.LiveMeter
 import spp.protocol.instrument.location.LiveSourceLocation
@@ -34,6 +35,7 @@ import spp.protocol.view.LiveViewEvent
 import spp.protocol.view.rule.MethodTimerAvgRule
 import spp.protocol.view.rule.MethodTimerCountRule
 
+@Disabled
 class MeterMethodTimerTest : ProbeIntegrationTest() {
 
     private fun doTest() {
