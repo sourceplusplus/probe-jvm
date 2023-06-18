25b7e63c3136dda5e438a7661e8df4a179e1625f
Brandon Fergerson <bfergerson@apache.org>
2023-06-17 19:06:24 -0500

    test: try

--- boot/src/test/kotlin/integration/meter/MeterTagTest.kt
+++ boot/src/test/kotlin/integration/meter/MeterTagTest.kt
@@ -24,7 +24,6 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import spp.protocol.instrument.LiveMeter
 import spp.protocol.instrument.location.LiveSourceLocation
@@ -34,7 +33,6 @@ import spp.protocol.view.LiveViewConfig
 import spp.protocol.view.LiveViewEvent
 import spp.protocol.view.rule.ViewRule
 
-@Disabled //todo: flaky test
 class MeterTagTest : ProbeIntegrationTest() {
 
     @Suppress("UNUSED_VARIABLE")
